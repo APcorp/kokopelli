88503aa150cddac11349f68774f5bcdfc5f1eb47
Theodore Liu <theodoretliu@gmail.com>
2016-05-14 16:03:49 -0500

    Final changes to Final Activity. It now shows name, quantity, and other stuff.

--- app/src/main/java/comapcorp/github/kokopelliorderform/FinalActivity.java
+++ app/src/main/java/comapcorp/github/kokopelliorderform/FinalActivity.java
@@ -7,6 +7,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Formatter;
 
 public class FinalActivity extends AppCompatActivity {
 
@@ -20,8 +21,14 @@ public class FinalActivity extends AppCompatActivity {
         TextView txtFinalName = (TextView) findViewById(R.id.finalName);
         TextView txtFinalId = (TextView) findViewById(R.id.finalId);
 
-        txtFinalName.setText(intent.getStringExtra("Name"));
-        txtFinalId.setText(intent.getStringExtra("ID"));
+
+        String nameString = "Name: " + intent.getStringExtra("Name");
+
+        if (txtFinalName != null) txtFinalName.setText(nameString);
+
+        String idString = "ID: " + intent.getStringExtra("ID");
+
+        if (txtFinalId != null) txtFinalId.setText(idString);
 
         ArrayList<OrderedItem> cart = intent.getParcelableArrayListExtra("cart");
 
@@ -29,13 +36,12 @@ public class FinalActivity extends AppCompatActivity {
 
         LinearLayout parentLin = (LinearLayout) findViewById(R.id.linFinalActivity);
 
-        System.out.println(cart.size());
+        int size = 0;
 
-        for (int i = 0; i < cart.size(); ++i) {
-            System.out.println("Blah" + cart.get(i).getItemName());
-        }
+        if (cart != null) size = cart.size();
 
-        for (int i = 0; i < cart.size(); ++i) {
+
+        for (int i = 0; i < size; ++i) {
 
             LinearLayout tempLayout = new LinearLayout(this);
 
@@ -43,22 +49,34 @@ public class FinalActivity extends AppCompatActivity {
             TextView itemName = new TextView(this);
             itemName.setText(cart.get(i).getItemName());
             itemName.setTextAppearance(this, android.R.style.TextAppearance_Large);
-            itemName.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            itemName.setLayoutParams(new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT));
 
-            /*LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) itemName.getLayoutParams();
+            LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) itemName.getLayoutParams();
 
             lp.weight = 1;
 
             itemName.setLayoutParams(lp);
-*/
+
             tempLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
             tempLayout.addView(itemName);
 
-            parentLin.addView(tempLayout);
+            TextView itemQuantity = new TextView(this);
+            itemQuantity.setText(String.valueOf(cart.get(i).getQuantity()));
+            itemQuantity.setTextAppearance(this, android.R.style.TextAppearance_Large);
+            itemQuantity.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+            tempLayout.addView(itemQuantity);
+            if (parentLin != null) parentLin.addView(tempLayout);
 
         }
 
-        parentLin.invalidate();
 
+        TextView total = (TextView) findViewById(R.id.txtFinalTotal);
+
+        Formatter format = new Formatter();
+
+        format.format("$%.2f", intent.getDoubleExtra("Total", 0.00));
+
+        if (total != null) total.setText(format.toString());
     }
 }
